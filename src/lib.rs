03241319d9a70b838eaacbad51dea3bfd215cb98
Ricardo Delfin <me@rdelfin.com>
2025-01-17 21:08:20 +0000

    Added terrible movement model

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,54 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use nalgebra::{Rotation2, Vector2};
+use std::time::Duration;
+
+#[derive(Default)]
+pub struct SimState {
+    pub vehicle_position: Vector2<f32>,
+    pub vehicle_speed: f32,
+    pub angle: f32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait Observer {
+    type Observation;
+    fn observe(&self, state: &SimState) -> Self::Observation;
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct Action {
+    accelerator: f32,
+    steering_curvature: f32,
+}
+
+pub struct Simulator {
+    state: SimState,
+    max_speed: f32,
+    max_accelerator: f32,
+    max_angle: f32,
+}
+
+impl Simulator {
+    pub fn new() -> Simulator {
+        Simulator {
+            state: SimState::default(),
+            max_speed: 10.,
+            max_accelerator: 0.5,
+            max_angle: 45.,
+        }
+    }
+
+    pub fn advance(&mut self, action: Action, delta_t: Duration) {
+        let delta_t_s = delta_t.as_secs_f32();
+        let capped_accelerator = action
+            .accelerator
+            .clamp(-self.max_accelerator, self.max_accelerator);
+        self.state.vehicle_speed += capped_accelerator * delta_t_s;
+        self.state.vehicle_speed = self
+            .state
+            .vehicle_speed
+            .clamp(-self.max_speed, self.max_speed);
+        self.state.vehicle_position += Vector2::new(self.state.vehicle_speed, 0.);
+    }
+
+    pub fn observe<O: Observer>(&self, observer: &O) -> O::Observation {
+        observer.observe(&self.state)
     }
 }
